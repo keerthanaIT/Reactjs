859d913fa6bfa59aebb0bec13e854d7e11daab0c
keerthana <keerthanam119@mail.com>
2019-04-12 16:38:56 +0530

    constructor with functions

--- Indecision_app/src/playground/es6-classes-1.js
+++ Indecision_app/src/playground/es6-classes-1.js
@@ -2,11 +2,17 @@ console.log("hi app.js is running");
 
 
 class Person {
-constructor(){
+  constructor(name= ' Anonymus'){
   this.name = name;
   
+  }
+  getGretting(){
+  return 'Hi, I am' + this.name + '!';
 }
 
 }
 const me = new Person("tommy");
 console.log(me.getGretting());
+
+const other = new.Person();
+console.log(other.getGretting());
