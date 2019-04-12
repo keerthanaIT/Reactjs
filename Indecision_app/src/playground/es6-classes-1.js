6072487ff5b3647b13670d9c97e91da42828f2b7
keerthana <keerthanam119@mail.com>
2019-04-12 16:54:26 +0530

    Example task in constructor task

--- Indecision_app/src/playground/es6-classes-1.js
+++ Indecision_app/src/playground/es6-classes-1.js
@@ -2,17 +2,20 @@ console.log("hi app.js is running");
 
 
 class Person {
-  constructor(name= ' Anonymus'){
+  constructor(name= ' Anonymus',age=0){
   this.name = name;
+  this.age=age;
   
   }
   getGretting(){
-  return 'Hi, I am' + this.name + '!';
+  return `Hi, I am  ${this.name}!`;
 }
-
+getDescript(){
+  return`${this.name} is ${this.age} years old.`;
 }
-const me = new Person("tommy");
-console.log(me.getGretting());
+}
+const me = new Person("tommy", 26);
+console.log(me.getDescript());
 
-const other = new.Person();
-console.log(other.getGretting());
+const other = new Person();
+console.log(other.getDescript());
