34bf30a2a45ef3c6b8a2e946f39b783a29af7086
keerthana <keerthanam119@mail.com>
2019-04-14 18:10:40 +0530

    hiding and shoing the details by clicking the button task

--- Indecision_app/src/playground/build-it-visible.js
+++ Indecision_app/src/playground/build-it-visible.js
@@ -1,6 +1,37 @@
 class VisibilityToggle extends React.Component {
+  constructor(probs){
+    super(probs);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
+handleToggleVisibility(){
+  this.setState((prevState) => {
+    return {
+      visibility:prevState.visibility
+    };
+  })
+}  
   render() {
-    return <p>test</p>;
+    return 
+    (
+      <div>
+      <h1>Visibility toggle</h1>
+      <button onClick={this.handleToggleVisibility}>
+      {visibility ? 'hide data' : 'sho data'}
+      </button>
+      {visibility && (
+        <div>
+        <p>This is the data </p>
+        </div>
+
+
+        )}
+      </div>
+
+
+      );
   }
 }
 ReactDOM.render(<VisibilityToggle />,document.getElementById('app'));
