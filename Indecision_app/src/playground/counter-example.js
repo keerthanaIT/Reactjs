73cf2418206cd4b6b6d3350cd7b11c350de63ee0
keerthana <keerthanam119@mail.com>
2019-04-14 16:55:10 +0530

    Adding methods to the button in counter app

--- Indecision_app/src/playground/counter-example.js
+++ Indecision_app/src/playground/counter-example.js
@@ -1,4 +1,18 @@
 class Counter extends React.Component{
+  handleAddOne(){
+    console.log('Adding  One');
+  }
+  handleMinusOne(){
+    console.log('Minus ONe by One');
+  }
+
+  handleReset(){
+    console.log("resetting the count");
+  }
+
+
+
+
   render(){
     return(
       <div>
