9e01677f0f1d6ce268ba11267c74c9e8472bc72d
keerthana <keerthanam119@mail.com>
2019-04-16 17:04:16 +0530

    importing and using validator in react

--- Indecision_app/src/app.js
+++ Indecision_app/src/app.js
@@ -1,11 +1,15 @@
-// import subract,{square , add} from './utilis.js'
-import isSenior ,{ isAdult , canDrink} from './person.js'
+// // import subract,{square , add} from './utilis.js'
+// import isSenior ,{ isAdult , canDrink} from './person.js'
 
 
-console.log("data app.js --------");
-// console.log(square(4));
-// console.log(add(10,10));
-// console.log(subract(20,10));
-console.log(isAdult(18));
-console.log(canDrink(21));
-console.log(isSenior(64));
+// console.log("data app.js --------");
+// // console.log(square(4));
+// // console.log(add(10,10));
+// // console.log(subract(20,10));
+// console.log(isAdult(18));
+// console.log(canDrink(21));
+// console.log(isSenior(64));
+
+import validator from 'validator';
+
+console.log(validator.isEmail('test@gmail.com'));
